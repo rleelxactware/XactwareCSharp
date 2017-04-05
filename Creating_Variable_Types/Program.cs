2628d69b9203fdddbf8d7946dd1035e6a3bb0d65
Russell <rlee@xactware.com>
2017-04-05 10:11:08 -0600

    testing

---
+++ Creating_Variable_Types/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_Variable_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\r\nListing out all 13 types of variables!!\r\n");
+
+            byte someByte = 15;
+            short someShort = 16;
+            int someInt = 17;
+            long someLong = 18;
+            sbyte someSbyte = 25;
+            ushort someUshort = 26;
+            uint someUint = 27;
+            ulong someUlong = 28;
+            char someChar = 'a';
+            decimal someDecimal = 14.1m;
+            float someFloat = 13.83F;
+            double someDouble = 12.33;
+            bool someBool = true;
+
+            Console.WriteLine(someByte);
+            Console.WriteLine(someShort);
+            Console.WriteLine(someInt);
+            Console.WriteLine(someLong);
+            Console.WriteLine(someSbyte);
+            Console.WriteLine(someUshort);
+            Console.WriteLine(someUint);
+            Console.WriteLine(someUlong);
+            Console.WriteLine(someChar);
+            Console.WriteLine(someDecimal);
+            Console.WriteLine(someFloat);
+            Console.WriteLine(someDouble);
+            Console.WriteLine(someBool);
+            Console.WriteLine("\n\rPress any key to continue ...");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ creatingVariables/variables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 2;
+
+            b = a;
+            a = -3;
+
+            Console.WriteLine("Variable a = " + a);
+            Console.WriteLine("Variable b = " + b);
+            Console.WriteLine("\r\nPress any key to continue ...");
+            
+            Console.ReadKey();
+        }
+    }
+}
